fc1ecca0b0c460a7270519f4e3136937b6c25727
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-06-27 20:43:07 +0200

    added new API controller (Users)

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]     // /api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]                   // GET method
+        public ActionResult<IEnumerable<AppUser>> GetUsers()            // if we do not return ActionResult, then we cannot return e.g. Bad Page error (404), etc. (but we still can return users)
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+
+    }
+}

62be935ca6031ef8990a4e43cf62cf18c421428a
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-06-29 19:36:29 +0200

    added CORS support in the API part

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 // ... we removed Swagger services from here
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 // ... we removed Swagger parts from here
 

94c3072ea0188f512f824971ea5c245b776e45d2
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-07-04 17:30:30 +0200

    added Angular-Bootstrap dropdown components to the navbar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,21 @@
+// Angular stuff:
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 // My personal import
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // end
+
+// Non-Angular stuff:
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+
+// My personal import
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+// end
 
 @NgModule({
   declarations: [
@@ -15,10 +23,12 @@ import { FormsModule } from '@angular/forms';
     NavComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

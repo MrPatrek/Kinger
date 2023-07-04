651b7f6175952b86a2476e6b461100e2c2a6c216
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-07-04 14:37:02 +0200

    now working with client: created a nav bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { HttpClientModule } from '@angular/common/http';
 // end
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,

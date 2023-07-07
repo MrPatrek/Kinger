bfc2755c277833df53f1c599a10bae1a468dfa6b
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-07-07 17:17:03 +0200

    some routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Observable, of } from 'rxjs';
 import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,22 +12,21 @@ import { User } from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { } // accountService is public because we want it to be accessible from nav.component.html
+  constructor(public accountService: AccountService, private router: Router) { } // accountService is public because we want it to be accessible from nav.component.html
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
+      next: _ => this.router.navigateByUrl('/members'), // _ is the same as () (for use when we do not have input params)
       error: error => console.log(error)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }

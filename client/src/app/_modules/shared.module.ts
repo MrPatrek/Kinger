3d1a543d50b2324ef71effb18b49dda606b95f62
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-07-14 15:50:00 +0200

    styling the member detailed page, continuation

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 
 
@@ -10,13 +11,15 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     CommonModule,     // always needs to be present in imported module (like this one, which is shared.module.ts, which is imported to app.module.ts)
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     })
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 export class SharedModule { }

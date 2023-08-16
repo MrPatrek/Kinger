1535532746cb96e1f4e0648331944056f0cac049
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-08-16 18:40:47 +0200

    added a Message controller

---
+++ API/Controllers/MessagesController.cs
@@ -0,0 +1,49 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class MessagesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMessageRepository _messageRepository;
+        private readonly IMapper _mapper;
+        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _messageRepository = messageRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var username = User.GetUsername();
+
+            if (username == createMessageDto.RecipientUsername.ToLower()) return BadRequest("You cannot send messages to yourself!");
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+
+            if (recipient == null) return NotFound();
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,         // Id property related to Recipient/Sender will be automatically filled by EF because of the first two lines in this (Message) object
+                Content = createMessageDto.Content
+            };
+
+            _messageRepository.AddMessage(message);             // don't forget that at this stage, the message is still not saved in the DB ... it is saved a line below:
+
+            if (await _messageRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));       // it's kinda "cheating", we just need to return at least smth, so that's okay our case
+
+            return BadRequest("Failed to send message");
+        }
+    }
+}

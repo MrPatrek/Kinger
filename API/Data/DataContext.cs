a8d771e561ef51a67352cdafb606ebf39a0af58b
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-08-22 19:01:24 +0200

    configuring the DbContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,15 +1,20 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser, AppRole, int,
+        IdentityUserClaim<int>, AppUserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>                     // we just indicate that we want an INT id
     {
         public DataContext(DbContextOptions options) : base(options)
         {
         }
 
-        public DbSet<AppUser> Users { get; set; }
+        // This one is already present in the inherited class:
+        // public DbSet<AppUser> Users { get; set; }
         public DbSet<UserLike> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
 
@@ -18,6 +23,18 @@ namespace API.Data
         {
             base.OnModelCreating(builder);          // explanation of base: https://stackoverflow.com/questions/2644316/what-really-is-the-purpose-of-base-keyword-in-c
 
+            builder.Entity<AppUser>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.User)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+            
+            builder.Entity<AppRole>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+            
             builder.Entity<UserLike>()
                 .HasKey(k => new {k.SourceUserId, k.TargetUserId});
             
--- API/Entities/AppUserRole.cs
+++ API/Entities/AppUserRole.cs
@@ -5,7 +5,7 @@ namespace API.Entities
     // // represents our Join Table between our Users and our Roles
     public class AppUserRole : IdentityUserRole<int>            // same as in AppUser, we want int id-s, not the string ones
     {
-        public AppUser User { get; set; }
-        public AppRole Role { get; set; }
+        public AppUser User { get; set; }       // UserId is inherited
+        public AppRole Role { get; set; }       // RoleId       -||-
     }
 }

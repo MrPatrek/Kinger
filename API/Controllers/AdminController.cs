575f7dc060e4b5ec7be38e970e6cd1eda18cc4cc
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-08-23 21:28:09 +0200

    editing user roles

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -32,6 +32,31 @@ namespace API.Controllers
             return Ok(users);
         }
 
+        [Authorize(Policy = "RequireAdminRole")]
+        [HttpPost("edit-roles/{username}")]             // this should have been a PUT request, but since we are also returning a user with roles, I decided to make it a POST request, although technically this should be a PUT request according to REST
+        public async Task<ActionResult> EditRoles(string username, [FromQuery] string roles)
+        {
+            if (string.IsNullOrEmpty(roles)) return BadRequest("You must select at least one role");
+
+            var selectedRoles = roles.Split(",").ToArray();             // our new roles
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null) return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);     // our old roles
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));     // add new roles
+
+            if (!result.Succeeded) return BadRequest("Failed to add to roles");
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));    // remove roles which were not present in query string
+
+            if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+
+            return Ok(await _userManager.GetRolesAsync(user));
+        }
+
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photos-to-moderate")]
         public ActionResult GetPhotosForModeration()

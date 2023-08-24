a20e4c46b7596af79d28cf6e47de58a7ee9c34ad
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-08-24 14:12:17 +0200

    added an admin component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 // end
 
 @NgModule({
@@ -53,7 +54,8 @@ import { MemberMessagesComponent } from './members/member-messages/member-messag
     PhotoEditorComponent,
     TextInputComponent,
     DatePickerComponent,
-    MemberMessagesComponent
+    MemberMessagesComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserAnimationsModule,

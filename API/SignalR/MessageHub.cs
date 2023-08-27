281072677030e55c5cebe639374137948c90da5d
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-08-27 21:16:09 +0200

    optimizing the messages

--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -25,22 +25,26 @@ namespace API.SignalR
             _messageRepository = messageRepository;
         }
 
-        public override async Task OnConnectedAsync()
+        public override async Task OnConnectedAsync()           // when we connect to some chat
         {
             var httpContext = Context.GetHttpContext();         // on the first connection from the client to the Message Hub, it will be an HTTP request (in order to init a WebSocket connection)
             var otherUser = httpContext.Request.Query["user"];
             var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await AddToGroup(groupName);
+            var group = await AddToGroup(groupName);
+
+            await Clients.Group(groupName).SendAsync("UpdatedGroup", group);
 
             var messages = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
 
-            await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
+            await Clients.Caller.SendAsync("ReceiveMessageThread", messages);
         }
 
-        public override async Task OnDisconnectedAsync(Exception exception)
+        public override async Task OnDisconnectedAsync(Exception exception)         // when we disconnect from some chat
         {
-            await RemoveFromMessageGroup();
+            var group = await RemoveFromMessageGroup();
+            await Clients.Group(group.Name).SendAsync("UpdatedGroup");          // in practice, this will be send to one user only ('cause if the user is removed from the group, our groups are only ever going to have two users, so it's going to basically send it to the other user (or the other user's clients))
+            // BTW, if there are no users in the group, SignarR won't even execute the method above, which is fine
             await base.OnDisconnectedAsync(exception);
         }
 
@@ -97,7 +101,7 @@ namespace API.SignalR
             return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
         }
 
-        private async Task<bool> AddToGroup(string groupName)
+        private async Task<Group> AddToGroup(string groupName)
         {
             var group = await _messageRepository.GetMessageGroup(groupName);
             var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
@@ -110,14 +114,21 @@ namespace API.SignalR
 
             group.Connections.Add(connection);
 
-            return await _messageRepository.SaveAllAsync();
+            if (await _messageRepository.SaveAllAsync()) return group;
+
+            throw new HubException("Failed to add to group");
         }
 
-        private async Task RemoveFromMessageGroup()
+        private async Task<Group> RemoveFromMessageGroup()
         {
-            var connection = await _messageRepository.GetConnection(Context.ConnectionId);
+            var group = await _messageRepository.GetGroupForConnection(Context.ConnectionId);
+            var connection = group.Connections.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
             _messageRepository.RemoveConnection(connection);
-            await _messageRepository.SaveAllAsync();
+
+
+            if (await _messageRepository.SaveAllAsync()) return group;
+
+            throw new HubException("Failed to remove from group");
         }
     }
 }

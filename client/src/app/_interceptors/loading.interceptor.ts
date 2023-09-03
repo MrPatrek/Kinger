d84be03413d3379ff8f5440ed38a829181158d35
Oleksandr Babenko <babenko.a2000@gmail.com>
2023-09-03 15:06:40 +0200

    creating an Angular production build

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -5,8 +5,9 @@ import {
   HttpEvent,
   HttpInterceptor
 } from '@angular/common/http';
-import { Observable, delay, finalize } from 'rxjs';
+import { Observable, delay, finalize, identity } from 'rxjs';
 import { BusyService } from '../_services/busy.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
@@ -17,8 +18,8 @@ export class LoadingInterceptor implements HttpInterceptor {
     this.busyService.busy();
     
     return next.handle(request).pipe(
-      delay(1000),                          // an artificial delay of 1 sec
-      finalize(() => {
+      (environment.production ? identity : delay(1000)),        // if not in production mode, return an artificial delay of 1 sec
+      finalize(() => {                                          // 'identity' function is the same as 'x => x' (that is, we just return the input parameter without any changes, which is our case since we don't want to introduce any artificial delays in case of production mode)
         this.busyService.idle()
       })
     )
